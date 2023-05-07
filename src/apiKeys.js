496aaa70fef2ea976f50e8c662b55555e7949f60
Sanay Kumar <sanaykumar062@gmail.com>
2023-05-07 15:29:13 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1f9c0e93df97d3b48201245ed4c82c2a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
